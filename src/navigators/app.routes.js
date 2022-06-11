9a375ec825b06e4d4022e422ea9a6baa743ccc1c
Rômulo Gadelha <romulogadelhaml@gmail.com>
2022-06-11 10:03:45 -0300

    feat: add ListProfessions screen in the navigator

--- src/navigators/app.routes.js
+++ src/navigators/app.routes.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 import Home from '../screens/Home';
 import ListProfessionals from '../screens/ListProfessionals';
+import ListProfessions from '../screens/ListProfessions';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ const AppRoutes = () => (
   <Navigator>
     <Screen name="Home" component={Home} options={{ headerShown: false }} />
     <Screen name="ListProfessionals" component={ListProfessionals} />
+    <Screen name="ListProfessions" component={ListProfessions} />
   </Navigator>
 );
 
