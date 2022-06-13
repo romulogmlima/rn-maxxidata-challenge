51eb202c7f9eca7cb8ed90455e03f6b53bd73567
Rômulo Gadelha <romulogadelhaml@gmail.com>
2022-06-13 10:55:16 -0300

    feat(screen): add Picker and MaskedInput components to the RegisterProfessional screen

--- src/screens/RegisterProfessional/index.js
+++ src/screens/RegisterProfessional/index.js
@@ -1,8 +1,13 @@
 import { useFormik } from 'formik';
 import React from 'react';
 
+import Button from '../../components/Buttons/Button';
 import Input from '../../components/Input';
+import MaskedInput from '../../components/MaskedInput';
+import Picker from '../../components/Picker';
 import Wrapper from '../../components/Wrapper';
+import { validationSchema } from '../../validations';
+import { BasicSwitch, ContainerSwitch, LabelSwitch } from './styles';
 
 const initialValues = {
   nome: '',
@@ -19,7 +24,7 @@ const RegisterProfessional = () => {
 
   const formik = useFormik({
     initialValues,
-    //validationSchema,
+    validationSchema,
     onSubmit,
   });
 
@@ -28,9 +33,11 @@ const RegisterProfessional = () => {
     touched,
     errors,
     handleChange,
+    handleBlur,
     isSubmitting,
     isValid,
     handleSubmit,
+    setFieldValue,
   } = formik;
 
   return (
@@ -38,39 +45,67 @@ const RegisterProfessional = () => {
       <Input
         label="Nome completo *"
         placeholder="Maria da Silva"
-        autoCapitalize="words"
         onChangeText={handleChange('nome')}
+        onBlur={handleBlur('nome')}
         value={values.nome}
         error={touched.nome && errors.nome}
+        autoCapitalize="words"
       />
-      <Input
+      <MaskedInput
         label="Telefone"
         placeholder="(XX) XXXXX-XXXX"
         onChangeText={handleChange('telefone')}
+        onBlur={handleBlur('telefone')}
         value={values.telefone}
         error={touched.telefone && errors.telefone}
         keyboardType="numeric"
+        type="cel-phone"
+        options={{
+          maskType: 'BRL',
+          withDDD: true,
+          dddMask: '(99) ',
+        }}
       />
       <Input
         label="E-mail"
         placeholder="email@email.com"
-        autoCapitalize="none"
         onChangeText={handleChange('email')}
+        onBlur={handleBlur('email')}
         value={values.email}
         error={touched.email && errors.email}
         keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <Picker
+        label="Profissão"
+        onValueChange={(value) => setFieldValue('tipoDeProfissional', value)}
+        error={touched.tipoDeProfissional && errors.tipoDeProfissional}
+        placeholder={{ label: 'Selecionar', value: '', key: 0 }}
+        items={[
+          { label: 'Médico', value: 'Médico', key: 1 },
+          {
+            label: 'Desenvolvedor de Sistemas',
+            value: 'Desenvolvedor de Sistemas',
+            key: 2,
+          },
+          { label: 'Motorista', value: 'Motorista', key: 3 },
+        ]}
+      />
+      <ContainerSwitch>
+        <LabelSwitch>Cadastro ativo</LabelSwitch>
+        <BasicSwitch
+          value={values.situacao}
+          onValueChange={(value) => setFieldValue('situacao', value)}
+        />
+      </ContainerSwitch>
+      <Button
+        title="Cadastrar"
+        onPress={handleSubmit}
+        isDisabled={!isValid || isSubmitting}
+        isLoading={isSubmitting}
       />
     </Wrapper>
   );
 };
 
 export default RegisterProfessional;
-
-//Nome
-//Telefone
-//Email
-
-//Tipo
-//Situação
-
-//label, placeholder, value, onChangeText, error
