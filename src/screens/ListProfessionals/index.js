50fb9644cab266fb55d5158105971f18ff922fb4
Rômulo Gadelha <romulogadelhaml@gmail.com>
2022-06-13 18:29:57 -0300

    feat(screen): add the Dialog component on the ListProfessionals and ListProfessions screens

--- src/screens/ListProfessionals/index.js
+++ src/screens/ListProfessionals/index.js
@@ -1,14 +1,27 @@
+import { useNavigation } from '@react-navigation/native';
 import React, { useMemo, useState } from 'react';
+import { Text } from 'react-native';
 
 import ProfessionalCard from '../../components/Cards/ProfessionalCard';
+import Dialog from '../../components/Dialog';
 import SearchBar from '../../components/SearchBar';
 import Wrapper from '../../components/Wrapper';
 import data from '../../mocks/professionals';
-import { Content, List } from './styles';
+import {
+  ContainerButtonsDialog,
+  Content,
+  List,
+  PrimaryButton,
+  PrimaryButtonTitle,
+  SecondaryButton,
+  SecondaryButtonTitle,
+} from './styles';
 
 const ListProfessionals = () => {
+  const navigation = useNavigation();
   const [search, setSearch] = useState('');
   const [listProfessionals, setListProfessionals] = useState(data);
+  const [isDialogVisible, setDialogVisible] = useState(false);
 
   const filteredListProfessionals = useMemo(() => {
     return listProfessionals.filter((item) =>
@@ -23,7 +36,17 @@ const ListProfessionals = () => {
           data={filteredListProfessionals}
           keyExtractor={(item) => item.id}
           showsVerticalScrollIndicator={false}
-          renderItem={({ item }) => <ProfessionalCard item={item} />}
+          renderItem={({ item }) => (
+            <ProfessionalCard
+              item={item}
+              onPressDelete={() => setDialogVisible(true)}
+              onPressEdit={() =>
+                navigation.navigate('RegisterProfessional', {
+                  initialValues: item,
+                })
+              }
+            />
+          )}
           ListHeaderComponent={
             <SearchBar
               value={search}
@@ -32,6 +55,17 @@ const ListProfessionals = () => {
             />
           }
         />
+        <Dialog visible={isDialogVisible}>
+          <Text>Deseja remover o registro?</Text>
+          <ContainerButtonsDialog>
+            <SecondaryButton onPress={() => setDialogVisible(false)}>
+              <SecondaryButtonTitle>Cancelar</SecondaryButtonTitle>
+            </SecondaryButton>
+            <PrimaryButton onPress={() => console.log('remover registro!')}>
+              <PrimaryButtonTitle>Remover</PrimaryButtonTitle>
+            </PrimaryButton>
+          </ContainerButtonsDialog>
+        </Dialog>
       </Content>
     </Wrapper>
   );
--- src/screens/ListProfessions/styles.js
+++ src/screens/ListProfessions/styles.js
@@ -7,3 +7,28 @@ export const Content = styled.View`
 `;
 
 export const List = styled.FlatList``;
+
+export const ContainerButtonsDialog = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 30px;
+`;
+
+export const PrimaryButton = styled.TouchableOpacity`
+  width: 100px;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  margin-left: 50px;
+  background-color: red;
+  border-radius: 10px;
+`;
+
+export const PrimaryButtonTitle = styled.Text``;
+
+export const SecondaryButton = styled(PrimaryButton)`
+  background-color: green;
+  margin-left: 0;
+`;
+
+export const SecondaryButtonTitle = styled.Text``;
