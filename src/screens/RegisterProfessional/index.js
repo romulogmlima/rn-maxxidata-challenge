ed82a0010c8cbb29ec14aadcdc8a598cdab02394
Rômulo Gadelha <romulogadelhaml@gmail.com>
2022-06-13 14:44:08 -0300

    feat(screen): add ScrollView into the RegisterProfessional screen

--- src/screens/RegisterProfessional/index.js
+++ src/screens/RegisterProfessional/index.js
@@ -1,5 +1,6 @@
 import { useFormik } from 'formik';
 import React from 'react';
+import { ScrollView } from 'react-native';
 
 import Button from '../../components/Buttons/Button';
 import Input from '../../components/Input';
@@ -42,68 +43,71 @@ const RegisterProfessional = () => {
 
   return (
     <Wrapper>
-      <Input
-        label="Nome completo *"
-        placeholder="Maria da Silva"
-        onChangeText={handleChange('nome')}
-        onBlur={handleBlur('nome')}
-        value={values.nome}
-        error={touched.nome && errors.nome}
-        autoCapitalize="words"
-      />
-      <MaskedInput
-        label="Telefone"
-        placeholder="(XX) XXXXX-XXXX"
-        onChangeText={handleChange('telefone')}
-        onBlur={handleBlur('telefone')}
-        value={values.telefone}
-        error={touched.telefone && errors.telefone}
-        keyboardType="numeric"
-        type="cel-phone"
-        options={{
-          maskType: 'BRL',
-          withDDD: true,
-          dddMask: '(99) ',
-        }}
-      />
-      <Input
-        label="E-mail"
-        placeholder="email@email.com"
-        onChangeText={handleChange('email')}
-        onBlur={handleBlur('email')}
-        value={values.email}
-        error={touched.email && errors.email}
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
-      <Picker
-        label="Profissão"
-        onValueChange={(value) => setFieldValue('tipoDeProfissional', value)}
-        error={touched.tipoDeProfissional && errors.tipoDeProfissional}
-        placeholder={{ label: 'Selecionar', value: '', key: 0 }}
-        items={[
-          { label: 'Médico', value: 'Médico', key: 1 },
-          {
-            label: 'Desenvolvedor de Sistemas',
-            value: 'Desenvolvedor de Sistemas',
-            key: 2,
-          },
-          { label: 'Motorista', value: 'Motorista', key: 3 },
-        ]}
-      />
-      <ContainerSwitch>
-        <LabelSwitch>Cadastro ativo</LabelSwitch>
-        <BasicSwitch
-          value={values.situacao}
-          onValueChange={(value) => setFieldValue('situacao', value)}
+      <ScrollView showsVerticalScrollIndicator={false} style={{ flex: 1 }}>
+        <Input
+          label="Nome completo *"
+          placeholder="Maria da Silva"
+          onChangeText={handleChange('nome')}
+          onBlur={handleBlur('nome')}
+          value={values.nome}
+          error={touched.nome && errors.nome}
+          autoCapitalize="words"
         />
-      </ContainerSwitch>
-      <Button
-        title="Cadastrar"
-        onPress={handleSubmit}
-        isDisabled={!isValid || isSubmitting}
-        isLoading={isSubmitting}
-      />
+        <MaskedInput
+          label="Telefone"
+          placeholder="(XX) XXXXX-XXXX"
+          onChangeText={handleChange('telefone')}
+          onBlur={handleBlur('telefone')}
+          value={values.telefone}
+          error={touched.telefone && errors.telefone}
+          keyboardType="numeric"
+          type="cel-phone"
+          options={{
+            maskType: 'BRL',
+            withDDD: true,
+            dddMask: '(99) ',
+          }}
+        />
+        <Input
+          label="E-mail"
+          placeholder="email@email.com"
+          onChangeText={handleChange('email')}
+          onBlur={handleBlur('email')}
+          value={values.email}
+          error={touched.email && errors.email}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Picker
+          label="Profissão"
+          onValueChange={(value) => setFieldValue('tipoDeProfissional', value)}
+          error={touched.tipoDeProfissional && errors.tipoDeProfissional}
+          placeholder={{ label: 'Selecionar', value: '', key: 0 }}
+          items={[
+            { label: 'Médico', value: 'Médico', key: 1 },
+            {
+              label: 'Desenvolvedor de Sistemas',
+              value: 'Desenvolvedor de Sistemas',
+              key: 2,
+            },
+            { label: 'Motorista', value: 'Motorista', key: 3 },
+          ]}
+        />
+        <ContainerSwitch>
+          <LabelSwitch>Cadastro ativo</LabelSwitch>
+          <BasicSwitch
+            value={values.situacao}
+            onValueChange={(value) => setFieldValue('situacao', value)}
+          />
+        </ContainerSwitch>
+
+        <Button
+          title="Cadastrar"
+          onPress={handleSubmit}
+          isDisabled={!isValid || isSubmitting}
+          isLoading={isSubmitting}
+        />
+      </ScrollView>
     </Wrapper>
   );
 };
