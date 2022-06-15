a6cba2683b0781082dad83201dc463fd0a7f4366
Rômulo Gadelha <romulogadelhaml@gmail.com>
2022-06-15 14:48:29 -0300

    refactor(navigation): set the header title of screens

--- src/navigators/app.routes.js
+++ src/navigators/app.routes.js
@@ -2,35 +2,47 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 
 import Home from '../screens/Home';
-import ListProfessionals from '../screens/ListProfessionals';
-import ListProfessions from '../screens/ListProfessions';
-import RegisterProfession from '../screens/RegisterProfession';
-import RegisterProfessional from '../screens/RegisterProfessional';
+import EditProfession from '../screens/Profession/Edit';
+import ListProfessions from '../screens/Profession/List';
+import RegisterProfession from '../screens/Profession/Register';
+import EditProfessional from '../screens/Professional/Edit';
+import ListProfessionals from '../screens/Professional/List';
+import RegisterProfessional from '../screens/Professional/Register';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 const AppRoutes = () => (
-  <Navigator>
+  <Navigator initialRouteName="Home">
     <Screen name="Home" component={Home} options={{ headerShown: false }} />
-    <Screen
-      name="ListProfessionals"
-      component={ListProfessionals}
-      options={{ title: 'Lista de Profissionais' }}
-    />
-    <Screen
-      name="ListProfessions"
-      component={ListProfessions}
-      options={{ title: 'Lista de Profissões' }}
-    />
     <Screen
       name="RegisterProfessional"
       component={RegisterProfessional}
       options={{ title: 'Cadastrar Profissional' }}
     />
     <Screen
+      name="ListProfessionals"
+      component={ListProfessionals}
+      options={{ title: 'Lista de Profissionais' }}
+    />
+    <Screen
+      name="EditProfessional"
+      component={EditProfessional}
+      options={{ title: 'Editar Profissional' }}
+    />
+    <Screen
       name="RegisterProfession"
       component={RegisterProfession}
       options={{ title: 'Cadastrar Profissão' }}
+    />
+    <Screen
+      name="EditProfession"
+      component={EditProfession}
+      options={{ title: 'Editar Profissão' }}
+    />
+    <Screen
+      name="ListProfessions"
+      component={ListProfessions}
+      options={{ title: 'Lista de Profissões' }}
     />
   </Navigator>
 );
