05b86a23fb824b55e73c31e2710a3efb5001a4fd
Rômulo Gadelha <romulogadelhaml@gmail.com>
2022-06-16 10:17:44 -0300

    refactor(component): change button color when it is enabled

--- src/components/Buttons/Button/styles.js
+++ src/components/Buttons/Button/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components/native';
 export const Container = styled.TouchableOpacity`
   width: 100%;
   height: 50px;
-  background-color: ${({ disabled }) => (disabled ? '#C7C7C7' : '#ff0000')};
+  background-color: ${({ disabled }) => (disabled ? '#C7C7C7' : '#232840')};
   justify-content: center;
   align-items: center;
   border-radius: 10px;
