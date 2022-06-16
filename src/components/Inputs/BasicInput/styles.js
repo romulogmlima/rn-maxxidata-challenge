4ed426bafc56fa2073ef41cd0ef6b7a278ba6bd2
Rômulo Gadelha <romulogadelhaml@gmail.com>
2022-06-16 20:32:09 -0300

    refactor(component): rename the Input component to BasicInput

--- src/components/Inputs/BasicInput/styles.js
+++ src/components/Inputs/BasicInput/styles.js
@@ -1,4 +1,4 @@
-import { TextInput } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -7,24 +7,24 @@ export const Container = styled.View`
 
 export const Label = styled.Text`
   margin-bottom: 5px;
-  font-size: 16px;
+  font-size: ${RFValue(14)}px;
   font-weight: bold;
   color: #232840;
 `;
 
-export const TextField = styled(TextInput).attrs(() => ({
+export const TextField = styled.TextInput.attrs(() => ({
   placeholderTextColor: '#BABABA',
 }))`
   width: 100%;
   padding: 15px 15px;
   color: #000;
   border-radius: 10px;
-  font-size: 16px;
+  font-size: ${RFValue(14)}px;
   background-color: #ffffff;
 `;
 
 export const ErrorMessage = styled.Text`
   color: red;
   margin-top: 5px;
-  font-size: 12px;
+  font-size: ${RFValue(12)}px;
 `;
