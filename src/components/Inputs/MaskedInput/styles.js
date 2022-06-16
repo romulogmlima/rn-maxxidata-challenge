9c40c36ad9b9cad00b736cb2ec490d864b559438
Rômulo Gadelha <romulogadelhaml@gmail.com>
2022-06-16 20:43:27 -0300

    refactor(component): move the MaskedInput and SearchBar components into the new folder 'Inputs'

--- src/components/Inputs/MaskedInput/styles.js
+++ src/components/Inputs/MaskedInput/styles.js
@@ -1,4 +1,5 @@
 import { TextInputMask } from 'react-native-masked-text';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -7,7 +8,7 @@ export const Container = styled.View`
 
 export const Label = styled.Text`
   margin-bottom: 5px;
-  font-size: 16px;
+  font-size: ${RFValue(14)}px;
   font-weight: bold;
   color: #232840;
 `;
@@ -17,14 +18,14 @@ export const TextField = styled(TextInputMask).attrs(() => ({
 }))`
   width: 100%;
   padding: 15px 15px;
-  background-color: #ffffff;
   color: #000;
   border-radius: 10px;
-  font-size: 16px;
+  font-size: ${RFValue(14)}px;
+  background-color: #ffffff;
 `;
 
 export const ErrorMessage = styled.Text`
   color: red;
   margin-top: 5px;
-  font-size: 12px;
+  font-size: ${RFValue(12)}px;
 `;
--- src/components/Inputs/SearchBar/styles.js
+++ src/components/Inputs/SearchBar/styles.js
@@ -1,14 +1,15 @@
 import { TextInput } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled(TextInput).attrs(() => ({
   placeholderTextColor: '#BABABA',
 }))`
   width: 100%;
-  padding: 18px 16px;
+  padding: 16px 16px;
   background-color: #ffffff;
   color: #000;
   border-radius: 10px;
   margin-bottom: 16px;
-  font-size: 16px;
+  font-size: ${RFValue(14)}px;
 `;
